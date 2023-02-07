710e4cc79812d4ef9420e860293e759e5c66bb03
ouhaddou nouredine <ouhaddounouredine@gmail.com>
2023-02-07 19:51:06 +0100

    Create setup_ubuntu.sh

---
+++ setup_ubuntu.sh
@@ -0,0 +1,9 @@
+# install vim
+sudo apt install vim
+
+# install curl
+sudo apt install curl
+
+# install ohmyzh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
